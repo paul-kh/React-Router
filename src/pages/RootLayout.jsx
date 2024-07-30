091db116c509977b48d29e04e2545d4e370e9a04
Paul Chheang <phalcheat@gmail.com>
2024-07-30 11:33:11 -0400

    Moved CSS styles for <main> to index.css for global styling

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-import classes from "./RootLayout.module.css";
 
 export default function RootLayout() {
   return (
@@ -8,7 +7,7 @@ export default function RootLayout() {
       <MainNavigation />
       {/* The <Outlet> component marks the location where
           the children of the RootLayout will reside */}
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
