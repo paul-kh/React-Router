d86fe67a3a02a8eea394b32ba48aaaa8232846f7
Paul Chheang <phalcheat@gmail.com>
2024-07-30 15:42:10 -0400

    Use the special 'relative' of <Link> to set relative path to route definition or to currently active path in the browser address bar

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function ProductDetailPage() {
   const params = useParams();
@@ -11,6 +11,16 @@ export default function ProductDetailPage() {
         inside of createBrowserRouter() in <App>, so params will have productId
         as an attribute */}
       <p>{params.productId}</p>
+      {/** The 'relative' prop of the below <Link> has 2 types of value:
+       * 1. 'path'  : path is resolved based on the currently active route in the address bar
+       * 2. 'route' : (default value) path is resolved based on route definition in
+       *              createBrowserRouter() of <App>
+       *
+       * Note: The 'to' prop = ".." means going up 1 level
+       */}
+      <Link to=".." relative="path">
+        Back
+      </Link>
     </>
   );
 }
