5b62a08f57b9c7662d4e38fe053a6aec6d96b102
Paul Chheang <phalcheat@gmail.com>
2024-07-30 00:06:12 -0400

    Rolled back to defining routes using object modes in 'createBrowserRouter()

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinitions);
-
-// const router = createBrowserRouter([
-//   // path is the part after the domain.
-//   // In https://example.com/products, path could be "/product" or "/"
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  // path is the part after the domain.
+  // In https://example.com/products, path could be "/product" or "/"
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
