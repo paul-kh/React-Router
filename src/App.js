bbc5830bee2e86a730d9fa757c2f482d1a816c1b
Paul Chheang <phalcheat@gmail.com>
2024-07-29 23:56:07 -0400

    Added 2nd route using 'createBrowserRouter()' and 'RouterProvider' of 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
 const router = createBrowserRouter([
   // path is the part after the domain.
   // In https://example.com/products, path could be "/product" or "/"
   { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
 ]);
 
 function App() {
