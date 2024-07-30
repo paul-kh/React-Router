6cb84b5920ca9351e3bab14e4e160df461ff0664
Paul Chheang <phalcheat@gmail.com>
2024-07-29 23:49:49 -0400

    Added first route '/' for Home Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([
+  // path is the part after the domain.
+  // In https://example.com/products, path could be "/product" or "/"
+  { path: "/", element: <HomePage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
