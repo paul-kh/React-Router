f4f1d3251adcdee5e9ea52c516b6f22eed195297
Paul Chheang <phalcheat@gmail.com>
2024-07-30 00:13:50 -0400

    Navigativing between pages with <Link>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        {/**
+         * The <Link> is different from <a> as <Link> is not sending
+         * page load request to server at all.
+         */}
+        Go to <Link to="/products"> the list of products </Link>.
+      </p>
+    </>
+  );
 }
